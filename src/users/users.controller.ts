17c7476541c55a38a9c7c7ce33756886d9d9957e
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-07-15 12:59:21 +0400

    Enhance user controller and service for profile and error handling
    Added endpoint to fetch current logged-in user's profile and updated route for fetching a single user. Improved UsersService methods to exclude current user from list, sort by updatedAt, and handle errors with appropriate exceptions.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,26 +3,40 @@ import { UsersService } from './users.service';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { Role } from '../auth/enums/role.enum';
 import { ApiOperation } from '@nestjs/swagger';
+import { GetActiveUser } from 'src/auth/decorators/getActiveUser';
+import { ActiveUserData } from 'src/auth/interface/active-user.interface';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  // Admin and Creator Routes
 
   @Get()
   @Roles(Role.Admin)
   @ApiOperation({
     summary: 'Get all users.',
   })
-  getAllUsers() {
-    return this.usersService.getAll();
+  getAllUsers(@GetActiveUser() user: ActiveUserData) {
+    return this.usersService.getAll(user.sub);
   }
 
-  @Get('/:id')
+  @Get('/user/:id')
   @Roles(Role.Admin)
   @ApiOperation({
     summary: 'Get single user.',
   })
   getSingleUser(@Param('id') id: number) {
     return this.usersService.getOne(id);
+  }
+
+  // Current logged-in user routes
+
+  @Get('/me/profile')
+  @ApiOperation({
+    summary: 'Fetch current logged-in user profile.',
+  })
+  async getCurrentUser(@GetActiveUser() user: ActiveUserData) {
+    return this.usersService.getOne(user.sub);
   }
 }
