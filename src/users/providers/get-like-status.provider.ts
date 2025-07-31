d78df0337e7f4a029e5402b0cc7dd47eb851bdc2
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-07-31 11:48:44 +0400

    Add endpoint to fetch like status for character
    Introduced GetLikeStatusProvider and integrated it into UsersService and UsersController. Added a new GET endpoint to retrieve whether the current user has liked a character and the total like count. Updated module providers and improved API documentation.

---
+++ src/users/providers/get-like-status.provider.ts
@@ -0,0 +1,48 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+import { Repository } from 'typeorm';
+import { Character } from 'src/characters/character.entity';
+
+@Injectable()
+export class GetLikeStatusProvider {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+
+    @InjectRepository(Character)
+    private readonly characterRepository: Repository<Character>,
+  ) {}
+
+  async getLikeStatus(characterId: number, userId: number) {
+    try {
+      const user = await this.userRepository.findOneBy({ id: userId });
+      if (!user) {
+        throw new NotFoundException('User not found!');
+      }
+
+      const character = await this.characterRepository.findOne({
+        where: { id: characterId },
+        relations: ['likedByUsers'],
+      });
+
+      if (!character) {
+        throw new NotFoundException('Character not found!');
+      }
+
+      const liked = character.likedByUsers.some((user) => user.id === userId);
+      const likeCount = character.likedByUsers.length;
+      return { liked, likeCount };
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new BadRequestException(
+        error.message || 'Oops...something went wrong!',
+      );
+    }
+  }
+}
